315ee40679ce59437c83ded2de43f3fc4bef7031
mohamed <mazn18675@gamil.com>
2024-11-02 14:56:41 +0300

    add note card

---
+++ lib/components/note_card.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class NoteCard extends StatelessWidget {
+  const NoteCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Colors.blue[400],
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Title",
+                  style: GoogleFonts.poppins(
+                      color: const Color(0xffffffff),
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30),
+                ),
+                IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.delete_outline,
+                      size: 30,
+                      color: Colors.white,
+                    ))
+              ],
+            ),
+            const SizedBox(
+              height: 14,
+            ),
+            Text(
+              "detail any thing you can write:my meeting start at 9:00 AM  ",
+              style: GoogleFonts.poppins(
+                  color: const Color.fromARGB(190, 0, 0, 0),
+                  fontWeight: FontWeight.w500,
+                  fontSize: 20),
+            ),
+            const SizedBox(
+              height: 14,
+            ),
+            Align(
+              alignment: Alignment.centerRight,
+              child: Text(
+                "2004 Oct",
+                style: GoogleFonts.poppins(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w500,
+                    fontSize: 20),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
