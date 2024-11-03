e69784bf3034cf96aa36263e91e04d28f25dc6b3
Samuel_Oluwaseyi <ejalonibuoluwaseyi@gmail.com>
2024-11-03 00:00:06 +0000

    Created note model

---
+++ lib/models/note_model.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Note {
+  final String title;
+  final String details;
+  final DateTime creationDate;
+
+  Note({
+    required this.title,
+    required this.details,
+    required this.creationDate,
+  });
+}
