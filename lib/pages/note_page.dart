757915da3f57c2f5dee150e2b1abb052173186b9
Samuel_Oluwaseyi <ejalonibuoluwaseyi@gmail.com>
2024-11-05 22:45:36 +0000

    Added elavated button and included function to pop up note

--- lib/pages/note_page.dart
+++ lib/pages/note_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:notes/components/note_appbar.dart';
 import 'package:notes/components/notes_listview.dart';
+import 'package:notes/components/add_note_botton_sheet.dart';
 
 class NotePage extends StatelessWidget {
   const NotePage({
@@ -12,6 +13,24 @@ class NotePage extends StatelessWidget {
     return Scaffold(
       appBar: noteAppBar(),
       body: NotesListview(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            builder: (context) => const AddNoteBottonSheet(),
+          );
+        },
+        child: const Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+        backgroundColor: Colors.black,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(30.0),
+          ),
+        ),
+      ),
     );
   }
 }
