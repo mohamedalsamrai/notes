de07c705bd9acf92e57f32695720bb2490fa11f5
mohamedalsamrai <st.compe.mohammed.a.f.awad@ced.nahrainuniv.edu.iq>
2024-11-06 02:32:01 +0300

    make card compressible

--- lib/components/note_card.dart
+++ lib/components/note_card.dart
@@ -5,60 +5,63 @@ class NoteCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.only(bottom: 16, left: 16, right: 16),
-      color: Colors.blue[400],
-      child: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text(
-                  "Title",
-                  style: TextStyle(
-                      color: Color(0xffffffff),
-                      fontWeight: FontWeight.bold,
-                      fontSize: 30,
-                      fontFamily: "Poppins"),
-                ),
-                IconButton(
-                    onPressed: () {},
-                    icon: const Icon(
-                      Icons.delete_outline,
-                      size: 30,
-                      color: Colors.white,
-                    ))
-              ],
-            ),
-            const SizedBox(
-              height: 14,
-            ),
-            const Text(
-              "detail any thing you can write:my meeting start at 9:00 AM  ",
-              style: TextStyle(
-                  color: Color.fromARGB(190, 0, 0, 0),
-                  fontWeight: FontWeight.w400,
-                  fontSize: 20,
-                  fontFamily: "Poppins"),
-            ),
-            const SizedBox(
-              height: 14,
-            ),
-            const Align(
-              alignment: Alignment.centerRight,
-              child: Text(
-                "2004 Oct",
+    return GestureDetector(
+      onTap: () {},
+      child: Card(
+        margin: EdgeInsets.only(bottom: 16, left: 16, right: 16),
+        color: Colors.blue[400],
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    "Title",
+                    style: TextStyle(
+                        color: Color(0xffffffff),
+                        fontWeight: FontWeight.bold,
+                        fontSize: 30,
+                        fontFamily: "Poppins"),
+                  ),
+                  IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.delete_outline,
+                        size: 30,
+                        color: Colors.white,
+                      ))
+                ],
+              ),
+              const SizedBox(
+                height: 14,
+              ),
+              const Text(
+                "detail any thing you can write:my meeting start at 9:00 AM  ",
                 style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.w500,
+                    color: Color.fromARGB(190, 0, 0, 0),
+                    fontWeight: FontWeight.w400,
                     fontSize: 20,
                     fontFamily: "Poppins"),
               ),
-            )
-          ],
+              const SizedBox(
+                height: 14,
+              ),
+              const Align(
+                alignment: Alignment.centerRight,
+                child: Text(
+                  "2004 Oct",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 20,
+                      fontFamily: "Poppins"),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
