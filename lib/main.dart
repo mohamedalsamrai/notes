52b9801fd0f06b24750bc17c1c504496c91e993d
mohamedalsamrai <st.compe.mohammed.a.f.awad@ced.nahrainuniv.edu.iq>
2024-11-07 17:59:19 +0300

    add init hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:notes/pages/note_page.dart';
 import 'package:notes/theme.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  await Hive.openBox("notes_box");
+
   runApp(const Note());
 }
 
@@ -15,5 +19,3 @@ class Note extends StatelessWidget {
         theme: themeApp(), debugShowCheckedModeBanner: false, home: NotePage());
   }
 }
-
-
